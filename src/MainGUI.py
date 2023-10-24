db41c053a20232ea313a9ab9b514b749dae7ac95
Terry Tosh <tmt103@txstate.edu>
2023-10-24 01:58:50 -0500

    Get user input and output location implemented

--- src/MainGUI.py
+++ src/MainGUI.py
@@ -22,16 +22,34 @@ class MainGUI(QMainWindow):
 
         # Connect 'Execute Action' button to handle_execute()
         self.execute_button.clicked.connect(self.handle_execute)
+
+        # Initialize instance variables
+        self.image_file = ""
+        self.output_directory = ""
     
-    # TODO: Implement this function
     def handle_input_file_select(self):
         print("handle_input_file_select() fucnction called...")
+
+        options = QFileDialog.ReadOnly # Force user to only selected already exisitng files
+        file_filter = "Images (*.jpg *.png)" # User can only select .jpg and .png file types
+
+        # Store path of selected image file
+        image_file, _ = QFileDialog.getOpenFileName(self, "Select Image File", "", file_filter, options=options)
+
+        if image_file:
+            self.image_file = image_file
+            self.input_file_label.setText(image_file) # Modify label to show path of selected image file
     
-    # TODO: Implement this function
     def handle_output_directory_select(self):
         print("handle_output_directory_select() fucnction called...")
+
+        options = QFileDialog.ShowDirsOnly # Show only existing user directories
+        output_directory = QFileDialog.getExistingDirectory(self, "Select Output Directory", options=options)
+
+        if output_directory:
+            self.output_directory = output_directory
+            self.output_directory_label.setText(output_directory)
     
-    # TODO: Implement this function
     def handle_execute(self):
 
         # Store user action selections from UI radio buttons
