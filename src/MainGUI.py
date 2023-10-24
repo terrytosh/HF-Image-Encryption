23d6987e845b0aa86a3a116a647e4253646e84e4
Terry Tosh <tmt103@txstate.edu>
2023-10-24 01:28:14 -0500

    UI buttons connected to handler functions

--- src/MainGUI.py
+++ src/MainGUI.py
@@ -10,3 +10,27 @@ class MainGUI(QMainWindow):
 
         # Display UI
         self.show()
+
+        # Quit app when 'Exit' QAction clicked in the toolbar
+        self.actionClose.triggered.connect(qApp.quit)
+
+        # Connect 'Select Input Fiie' button to handle_input_file_select()
+        self.select_input_button.clicked.connect(self.handle_input_file_select)
+
+        # Connect 'Select Output Directory' button to handle_output_directory_select()
+        self.select_output_button.clicked.connect(self.handle_output_directory_select)
+
+        # Connect 'Execute Action' button to handle_execute()
+        self.execute_button.clicked.connect(self.handle_execute)
+    
+    # TODO: Implement this function
+    def handle_input_file_select(self):
+        print("handle_input_file_select() fucnction called...")
+    
+    # TODO: Implement this function
+    def handle_output_directory_select(self):
+        print("handle_output_directory_select() fucnction called...")
+    
+    # TODO: Implement this function
+    def handle_execute(self):
+        print("handle_execute() fucnction called...")
