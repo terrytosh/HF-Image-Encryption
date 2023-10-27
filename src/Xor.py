ca0bb6dc2cfc8822677e938d4430f50cacb6f8b1
Terry Tosh <tmt103@txstate.edu>
2023-10-26 20:45:59 -0500

    XOR encryption with hardcoded key implemented

--- src/Xor.py
+++ src/Xor.py
@@ -7,23 +7,25 @@ class Xor:
         self.key = key
 
     def encrypt(self):
-        img = Image.open(self.image_file)
-        img_data = img.tobytes()
+        image = Image.open(self.image_file)
+        pixel_data = list(image.getdata())
+        encrypted_pixel_data = []
 
-        key_length = len(self.key)
-        # Ensure the key matches the image data length
-        if key_length < len(img_data):
-            # Pad the key with zeros to match the image data length
-            self.key = self.key.ljust(len(img_data), '\x00')
-        elif key_length > len(img_data):
-            # Truncate the key to match the image data length
-            self.key = self.key[:len(img_data)]
-        else:
-            # Key length matches the image data length
-            self.key = self.key
+        byte_key = 0xAA
+
+        for pixel in pixel_data:
+            encrypted_pixel = []
+            for byte in pixel:
+                encrypted_byte = byte ^ byte_key
+                encrypted_pixel.append(encrypted_byte)
+            encrypted_pixel_data.append(tuple(encrypted_pixel))
         
-        self.byte_key = self.key.encode("utf-8")
-        encrypted_data = bytes(img_byte ^ key_byte for img_byte, key_byte in zip(img_data, self.byte_key))
-        encrypted_image = Image.frombytes(img.mode, img.size, encrypted_data)
-        encrypted_image_path = self.output_directory + "/encrypted_image.jpg"
-        encrypted_image.save(encrypted_image_path)
+        # Create a new image with the encrypted pixel data
+        encrypted_image = Image.new(image.mode, image.size)
+        encrypted_image.putdata(encrypted_pixel_data)
+
+        # Save the encrypted image
+        encrypted_image.save("\\Users\\16825\\Pictures\\Encrypted\\encrypted_image.png")
+
+        image.close()
+        encrypted_image.close()
